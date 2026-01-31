1773445535c0ccbed1fa9a868dc8941d45d15a1d
Mehul Kaushik <97294636+Epicguest97@users.noreply.github.com>
2026-02-01 03:59:50 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -140,7 +140,7 @@ export default function App() {
                   onTouchEnd={handlePressEnd}
                 >
                   <img 
-                    src="https://media.licdn.com/dms/image/v2/D4E03AQENq4GdHViyDg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1731440629073?e=1759968000&v=beta&t=m0yoPJzuKOy2X_1X34zaPbouKq7sYwFpfFC20uJhK54" 
+                    src="https://i.ibb.co/qF2V16g3/Whats-App-Image-2026-01-17-at-14-46-20.jpg" 
                     alt="Profile" 
                     className="w-full h-full object-cover"
                   />
